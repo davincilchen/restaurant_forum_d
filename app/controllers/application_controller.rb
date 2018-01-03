0845aa7f3c07fc2254a4b3ed8910bcd2e6d5cd49
davincilchen <doki2099@gmail.com>
2018-01-03 18:18:01 +0800

    add Admin::BaseController and move authenticate_admin here

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,5 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
 
-  private
-   
-  def authenticate_admin
-    unless current_user.admin?
-      flash[:alert] = "Not admin, Not allow!!"
-      redirect_to root_path
-    end
-  end
+
 end

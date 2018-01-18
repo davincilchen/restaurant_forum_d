ade5e0f48010e7e460b50ea9b45123e8dd9d27e5
davincilchen <doki2099@gmail.com>
2018-01-18 17:34:10 +0800

    add show method in categories_contorller.rb

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,9 @@
 class CategoriesController < ApplicationController
+
+  def show
+    @categories = Category.all
+    @category = Category.find(param[:id])
+    @restaurants = @category.restaurants.page(param[:page]).per(9)
+
+  end
 end

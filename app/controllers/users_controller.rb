d6ae6bf4088960d720698441e84abc6a3cae705a
davincilchen <doki2099@gmail.com>
2018-02-24 18:17:12 +0800

    show the number of commented restaurants on profile page and don't show repeated restaurant

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
 
 
   def show
-    @commented_restaurants = @user.restaurants
+    @commented_restaurants = @user.restaurants.uniq
   end
 
   def edit

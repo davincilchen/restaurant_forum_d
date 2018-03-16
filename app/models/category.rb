d2a7f8d82595b82021783d6912a7de419d1c6070
davincilchen <doki2099@gmail.com>
2018-03-16 16:43:35 +0800

    test dependent: :restrict_with_exception

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,4 +3,5 @@ class Category < ApplicationRecord
 
    # 如果分類下已有餐廳，就不允許刪除分類（刪除時拋出 Error）
   has_many :restaurants, dependent: :restrict_with_error
+  #has_many :restaurants, dependent: :restrict_with_exception
 end

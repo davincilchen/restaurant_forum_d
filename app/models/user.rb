8c47c4449fba64567fb657cae491ebe58d1e4ed3
davincilchen <doki2099@gmail.com>
2018-03-19 23:07:39 +0800

    setup self-referential relationships within users table through followships

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,6 +21,12 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :liked_restaurants, through: :likes, source: :restaurant
 
+  # 「使用者追蹤使用者」的 self-referential relationships 設定
+  # 不需要另加 source，Rails 可從 Followship Model 設定來判斷 followings 指向 User Model
+  has_many :followships, dependent: :destroy
+  has_many :followings, through: :followships
+
+
  # admin? 讓我們用來判斷單個user是否有 admin 角色，列如：current_user.admin?
   def admin?
     self.role == "admin"

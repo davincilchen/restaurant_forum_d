1a46301ed0e976a47202abce78be981bb1cca25b
davincilchen <doki2099@gmail.com>
2018-03-19 19:06:36 +0800

    update favorites_count using counter_cache

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -31,7 +31,6 @@ class RestaurantsController < ApplicationController
     #@restaurant.favorites.create!(user_id: current_user.id)
     #Favorite.create(restaurant: @restaurant, user: current_user)
     #current_user.favorites.create(restaurant: @restaurant)
-    @restaurant.count_favorites
     redirect_back(fallback_location: root_path)  # 導回上一頁
   end
 
@@ -43,7 +42,6 @@ class RestaurantsController < ApplicationController
     if favorites.destroy_all
 
     end
-    @restaurant.count_favorites
     redirect_back(fallback_location: root_path)
   end
 

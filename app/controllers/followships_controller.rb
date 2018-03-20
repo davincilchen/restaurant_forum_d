0898cea0c65b8786246680bc5b93cdb52a4098e0
davincilchen <doki2099@gmail.com>
2018-03-20 15:59:45 +0800

    modify user index page with follow button

--- app/controllers/followships_controller.rb
+++ app/controllers/followships_controller.rb
@@ -1,2 +1,9 @@
 class FollowshipsController < ApplicationController
+  def create
+    puts "FollowshipsController ~~ create"
+  end
+
+  def destroy
+    puts "FollowshipsController ~~ destroy"
+  end
 end

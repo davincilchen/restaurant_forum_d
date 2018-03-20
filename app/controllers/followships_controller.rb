1b98bf322e30dfdef518865c3361c0be6e32970d
davincilchen <doki2099@gmail.com>
2018-03-20 17:35:12 +0800

    add create action for followship record

--- app/controllers/followships_controller.rb
+++ app/controllers/followships_controller.rb
@@ -1,9 +1,21 @@
 class FollowshipsController < ApplicationController
+
   def create
-    puts "FollowshipsController ~~ create"
+    puts "FollowshipsController ~~ create ~~"
+    # 需要設定前端的 link_to，在發出請求時送進 following_id
+    @followship = current_user.followships.build(following_id: params[:following_id])
+
+    if @followship.save
+      flash[:notice] = "Successfully followed"
+      redirect_back(fallback_location: root_path)
+    else
+      flash[:alert] = @followship.errors.full_messages.to_sentence
+      redirect_back(fallback_location: root_path)
+    end
+
   end
 
   def destroy
-    puts "FollowshipsController ~~ destroy"
+    puts "FollowshipsController ~~ destroy ~~"
   end
 end

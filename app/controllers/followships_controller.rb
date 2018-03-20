2595835e37ee644018f46939ecd17048f4ccd407
davincilchen <doki2099@gmail.com>
2018-03-20 20:28:55 +0800

    add unfollow button on user index page and destroy action

--- app/controllers/followships_controller.rb
+++ app/controllers/followships_controller.rb
@@ -1,21 +1,26 @@
 class FollowshipsController < ApplicationController
 
   def create
-    puts "FollowshipsController ~~ create ~~"
     # 需要設定前端的 link_to，在發出請求時送進 following_id
     @followship = current_user.followships.build(following_id: params[:following_id])
 
     if @followship.save
       flash[:notice] = "Successfully followed"
-      redirect_back(fallback_location: root_path)
     else
       flash[:alert] = @followship.errors.full_messages.to_sentence
-      redirect_back(fallback_location: root_path)
     end
-
+    redirect_back(fallback_location: root_path)
   end
 
   def destroy
-    puts "FollowshipsController ~~ destroy ~~"
+    # where 會回傳物件集合(Array)，如果有需要串接可取出單一物件的方法如 first
+    @followship = current_user.followships.where(following_id: params[:id])
+    if @followship.size == 0
+      flash[:alert] = "No followship for user is destroyed"
+    else
+      @followship.destroy_all
+      flash[:alert] = "Followship destroyed"
+    end
+    redirect_back(fallback_location: root_path)
   end
 end
